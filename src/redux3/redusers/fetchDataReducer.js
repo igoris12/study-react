a56b09b872cedf0b1e5653639f8e1636b90d7805
Igoris Ivanovas <igoris.x@inbox.lt>
2021-12-23 20:03:24 +0200

    reducer done

---
+++ src/redux3/redusers/fetchDataReducer.js
@@ -0,0 +1,35 @@
+import { FETCH_DATA, FETCH_SUCCESS, FETCH_ERROR } from '../actionsType';
+
+const initionState = {
+    loding: false,
+    data: {},
+    error: ''
+}
+
+const fetchReducer = (state = initionState, action) => {
+    switch (action.type) {
+        case FETCH_DATA:
+            return {
+                loding: true,
+                data: {},
+                error: ''
+            }
+
+        case FETCH_SUCCESS:
+            return {
+                loding: false,
+                data: { ...action.deploy },
+                error: ''
+            }
+
+        case FETCH_ERROR:
+            return {
+                loding: false,
+                data: {},
+                error: action.deploy
+            }
+
+        default:
+            return state;
+    }
+} 
