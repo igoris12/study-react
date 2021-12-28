9e63a65f6d6f42e1ad52d89da547976126bd022b
Igoris Ivanovas <igoris.x@inbox.lt>
2021-12-28 21:11:10 +0200

    display user list

--- src/components/DataContainer.jsx
+++ src/components/DataContainer.jsx
@@ -1,21 +1,29 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchUsers } from '../redux5/features/users/usersSlice';
+import UserListItem from './UserListItem';
 
 const DataContainer = () => {
   const users = useSelector((state) => state.users);
   const dispatch = useDispatch();
-  console.log(users);
 
   return (
     <div>
       <h2 className="header">Fetch data to redux store</h2>
       <ul className="listContainer">
+        <button className="but" onClick={() => dispatch(fetchUsers())}>
+          Fetch data
+        </button>
         <div className="listHeader">
           <span className="idTitle">ID</span>
           <span className="usernameTitle">Username</span>
           <span className="emailTitle">Email</span>
         </div>
-        <button onClick={() => dispatch(fetchUsers())}>test</button>
+
+        {users.list.length
+          ? users.list.map((user) => {
+              return <UserListItem key={user.id} userData={user} />;
+            })
+          : null}
       </ul>
     </div>
   );
