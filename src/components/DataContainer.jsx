43b9e2b0a3aca19a8e77294435dd62e203d59186
Igoris Ivanovas <igoris.x@inbox.lt>
2023-11-28 19:01:40 +0100

    == to ===

--- src/components/DataContainer.jsx
+++ src/components/DataContainer.jsx
@@ -19,9 +19,9 @@ const DataContainer = () => {
           <span className="emailTitle">Email</span>
         </div>
 
-        {users.status == 'loading' && <h1>Loading...</h1>}
-        {users.status == 'rejected' && <h1>{users.error}</h1>}
-        {users.list && users.status == 'resolved'
+        {users.status === 'loading' && <h1>Loading...</h1>}
+        {users.status === 'rejected' && <h1>{users.error}</h1>}
+        {users.list && users.status === 'resolved'
           ? users.list.map((user) => {
               return <UserListItem key={user.id} userData={user} />;
             })
